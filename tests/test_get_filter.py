f99fbe46fde8f62115a8e0d3a03a1fbad33366b1
Jer-Tran <79068593+Jer-Tran@users.noreply.github.com>
2023-12-25 14:01:09 +1100

    Revert "Reading in Files"

--- tests/test_get_filter.py
+++ tests/test_get_filter.py
@@ -3,34 +3,40 @@ import pytest
 
 # May change this funciton to accept csv files as well, since it would be reasonably to produce the contents on excel then export
 
+@pytest.mark.skip(reason="currently unimplemented")
 def test_standard(): # If the filters are changed to work better with the output tests, don't forget to change here
-    filters = get_filters('culvert-scraper/tests/filters/filter1.txt')
+    filters = get_filters('tesst/filters/filter1.txt')
     assert filters == ['a', 'b', 'c']
     assert sorted(filters) == filters
-    assert len(set(filters)) == len(filters)
+    assert set(filters) == filters
 
+@pytest.mark.skip(reason="currently unimplemented")
 def test_order():
-    filters = get_filters('culvert-scraper/tests/filters/filter2.txt')
+    filters = get_filters('tests/filters/filter2.txt')
     assert filters == ['d', 'e', 'f']
     assert sorted(filters) == filters
-    assert len(set(filters)) == len(filters)
+    assert set(filters) == filters
 
 # file doesn't exist
+@pytest.mark.skip(reason="currently unimplemented")
 def test_bad_path():
     with pytest.raises(Exception):
         get_filters('notapath')
 
 # If specific file not specified
+@pytest.mark.skip(reason="currently unimplemented")
 def test_folder():
     with pytest.raises(Exception):
         get_filters('tests/filters')
 
 # Gets a text file that can be read, but the contents aren't in a valid format
 # Maybe this would get expanded upon further
+@pytest.mark.skip(reason="currently unimplemented")
 def test_invalid_format():
     with pytest.raises(Exception):
         get_filters('tests/filters/badformat.txt')
 
+@pytest.mark.skip(reason="currently unimplemented")
 def test_invalid_content():
     with pytest.raises(Exception):
         get_filters('tests/filters/badcontent.txt')
