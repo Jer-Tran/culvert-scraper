2741526feca952e94ce8d01a279c89e48e84082e
Jer-Tran <jeremy.tran83@gmail.com>
2023-12-27 22:24:31 +1100

    added writing output file

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,4 +1,4 @@
-from scraper import main
+from scraper import main, write_output
 import pytest
 from os.path import isfile 
 
@@ -25,6 +25,41 @@ def test_empty_by_filter():
     main(args)
 
     assert not isfile('output.csv')
+
+def test_invalid_write_variable():
+    with pytest.raises(Exception):
+        write_output("Accent")
+
+def test_invalid_write_list_format():
+    with pytest.raises(Exception):
+        write_output([(1,'a'),(2,'b'),(3,'c')])
+
+def test_write_empty():
+    data = []
+    write_output(data)
+
+    assert isfile('output.csv')
+    with open('output.csv', 'r') as f:
+        output = f.read()
+    
+    assert output == '''name,score
+'''
+
+
+def test_output_write():
+    data = [ ('a', 1), ('b', 10), ('c', 100) ] 
+    write_output(data)
+
+    assert isfile('output.csv')
+    with open('output.csv', 'r') as f:
+        output = f.read()
+    
+    assert output == '''name,score
+a,1
+b,10
+c,100
+'''
+
 
 @pytest.mark.skip(reason="currently unimplemented")
 def test_standard():
